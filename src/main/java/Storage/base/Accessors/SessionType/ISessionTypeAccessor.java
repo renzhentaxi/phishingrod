330014aac42fab6edaf6c04012ab3a508cc841a3
Renzh <renzhentaxibaerde@gmail.com>
2018-03-06 13:06:10 -0500

    started to work on sessionTypeAccessor

--- src/main/java/Storage/base/Accessors/SessionType/ISessionTypeAccessor.java
+++ src/main/java/Storage/base/Accessors/SessionType/ISessionTypeAccessor.java
@@ -2,9 +2,17 @@ package Storage.base.Accessors.SessionType;
 
 import Entities.SessionTypes.ISessionType;
 import Entities.SessionTypes.ISessionTypeEntity;
+import Storage.base.Accessors.Exceptions.EntityDoesNotExistException;
 import Storage.base.Accessors.IAccessor;
 
 public interface ISessionTypeAccessor extends IAccessor<ISessionTypeEntity, ISessionType>
 {
-
+    /**
+     * retrieve the sessionType with the given name
+     *
+     * @param name the name of the sessionType
+     * @return the sessionType
+     * @throws EntityDoesNotExistException if there are not sessionType with the given name.
+     */
+    ISessionTypeEntity getByName(String name);
 }
--- src/main/java/Storage/base/Accessors/SessionType/SessionTypeAccessor.java
+++ src/main/java/Storage/base/Accessors/SessionType/SessionTypeAccessor.java
@@ -2,11 +2,25 @@ package Storage.base.Accessors.SessionType;
 
 import Entities.SessionTypes.ISessionType;
 import Entities.SessionTypes.ISessionTypeEntity;
+import org.jdbi.v3.core.Jdbi;
 
 public class SessionTypeAccessor implements ISessionTypeAccessor
 {
+    private Jdbi jdbi;
+    private String getQuery;
+    private String getByNameQuery;
+    private String addQuery;
+    private String updateQuery;
+
+
     @Override
     public ISessionTypeEntity get(int id)
+    {
+        return null;
+    }
+
+    @Override
+    public ISessionTypeEntity getByName(String name)
     {
         return null;
     }
@@ -22,4 +36,6 @@ public class SessionTypeAccessor implements ISessionTypeAccessor
     {
 
     }
+
+
 }
