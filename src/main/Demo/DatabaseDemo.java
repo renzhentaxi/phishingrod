c9190f26acde04f8a339b3b741a2f5a377f66570
Renzh <renzhentaxibaerde@gmail.com>
2018-03-18 19:21:42 -0400

    renamed the test.db to dev.db

--- src/main/Demo/DatabaseDemo.java
+++ src/main/Demo/DatabaseDemo.java
@@ -1,27 +1,25 @@
-/*import Entities.Users.IUserEntity;
+import Entities.Users.IUserEntity;
 import Entities.Users.User;
 import Storage.Sqlite.DaggerSqliteStorageManager;
-import Storage.base.Accessors.Sender.old.SenderAccessor;
-import Storage.base.Accessors.User.UserAccessor;
 import Storage.base.StorageManager;
 
-public class DatabaseTest {
+public class DatabaseDemo
+{
     public static void main(String[] args) {
-        String databaseLocation = System.getProperty("user.dir") + "/data/database/test.db";
-
+        String databaseLocation = System.getProperty("user.dir") + "/data/database/dev.db";
 
         StorageManager storageManager = DaggerSqliteStorageManager.builder().databaseUrl(databaseLocation).build();
 
-        UserAccessor ua2 = storageManager.getUserAccessor();
-        SenderAccessor sa = storageManager.getSenderAccessor();
+        IUserAccessor userAccessor = storageManager.getUserAccessor();
+        ISenderAccessor senderAccessor = storageManager.getSenderAccessor();
 
-//        User userData = new User("taxi", "boi", "245", "taxi@yahoo.com");
-        IUserEntity user = ua2.getByEmail("gmail.com");
+        User userData = new User("taxi", "boi", "245", "taxi@yahoo.com");
+        IUserEntity user = userAccessor.getByEmail("gmail.com");
         user.setLastName("lastName");
-        ua2.update(user);
+        userAccessor.update(user);
 
         User userData = new User("abc", "fda", "342", "taxi@fajs.com");
-        IUserEntity user2 = ua2.add(userData);
+        IUserEntity user2 = userAccessor.add(userData);
 
 //        Sender senderData = new Sender(user, "password", "gmail");
 //
@@ -32,4 +30,3 @@ public class DatabaseTest {
 //        System.out.println(sender.getUser().getEmailAddress());
     }
 }
-*/
