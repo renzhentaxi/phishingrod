56f417b9969740b72c4d990e6be629dfd13c701c
Renzh <renzhentaxibaerde@gmail.com>
2018-03-18 14:59:43 -0400

    tiny modification to the exception message

--- src/main/java/Storage/base/Accessors/Exceptions/EntityAlreadyExistException.java
+++ src/main/java/Storage/base/Accessors/Exceptions/EntityAlreadyExistException.java
@@ -4,7 +4,7 @@ public class EntityAlreadyExistException extends RuntimeException {
 
     public EntityAlreadyExistException(String entityType, String identifierName, String identifierValue, Object object)
     {
-        super("\nA " + entityType + " with the same " + identifierName + "(" + identifierValue + ") already exist in the database." +
+        super("\nA " + entityType + " with the " + identifierName + " " + identifierValue + " already exist in the database." +
                 "\nOffending " + entityType + " :" + object.toString());
     }
 
