5166abb1aac24e682ed845ddae06f5a1802bf54c
Renzh <renzhentaxibaerde@gmail.com>
2018-03-18 20:33:38 -0400

    a simple class to use to access an instance of storageManager instead of instaniating DaggerSqliteStorageManager directly.

---
+++ src/main/java/Storage/StorageManager.java
@@ -0,0 +1,12 @@
+package Storage;
+
+import Storage.Sqlite.DaggerSqliteIStorageManager;
+import Storage.base.IStorageManager;
+
+public class StorageManager
+{
+    public static IStorageManager newInstance(String databaseLocation)
+    {
+        return DaggerSqliteIStorageManager.builder().databaseUrl(databaseLocation).build();
+    }
+}
