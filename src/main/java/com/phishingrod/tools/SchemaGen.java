77e024606f41ced5845593f5964aadb3c2823751
renzhentaxibaerde <renzhentaxibaerde@gmail.com>
2018-04-04 22:21:58 -0400

    Fixed bug: now generates sql dml again.
    Other:
    moved the configuration into a final string instead of hard coding it.
    change access level static methods to private

--- src/main/java/com/phishingrod/tools/SchemaGen.java
+++ src/main/java/com/phishingrod/tools/SchemaGen.java
@@ -6,9 +6,7 @@ import org.hibernate.service.ServiceRegistry;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
 import org.hibernate.tool.schema.TargetType;
 import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.type.filter.AnnotationTypeFilter;
 
 import javax.persistence.Entity;
@@ -18,24 +16,25 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
 
-@EntityScan("domain")
-@Configuration
 public class SchemaGen
 {
+    private final static String fileName = "db-schema.sql";
+    private final static String domainPackage = "com.phishingrod.domain";
+
     public static void main(String[] args)
     {
         Map<String, String> settings = getSettings();
         ServiceRegistry sr = new StandardServiceRegistryBuilder().applySettings(settings).build();
         MetadataSources metadata = getMetadata(sr);
-        File file = new File("db-schema.sql");
+        File file = new File(fileName);
         file.delete();
 
-        export("db-schema.sql", metadata, sr);
+        export(fileName, metadata, sr);
         System.exit(0);
 
     }
 
-    public static Map<String, String> getSettings()
+    private static Map<String, String> getSettings()
     {
         java.util.logging.Logger.getLogger("org.hibernate").setLevel(Level.OFF);
         HashMap<String, String> settings = new HashMap<>();
@@ -52,32 +51,30 @@ public class SchemaGen
 
     private static ClassPathScanningCandidateComponentProvider createComponentScanner()
     {
-        // Don't pull default filters (@Component, etc.):
         ClassPathScanningCandidateComponentProvider provider
                 = new ClassPathScanningCandidateComponentProvider(false);
         provider.addIncludeFilter(new AnnotationTypeFilter(Entity.class));
         return provider;
     }
 
-    public static MetadataSources getMetadata(ServiceRegistry sr)
+    private static MetadataSources getMetadata(ServiceRegistry sr)
     {
         MetadataSources metadata = new MetadataSources(sr);
         ClassPathScanningCandidateComponentProvider provider = createComponentScanner();
-        for (BeanDefinition beanDefinition : provider.findCandidateComponents("com.phishingrod.persistence.domain"))
+        for (BeanDefinition beanDefinition : provider.findCandidateComponents(domainPackage))
         {
             metadata.addAnnotatedClassName(beanDefinition.getBeanClassName());
         }
-//        metadata.addAnnotatedClassName()
         return metadata;
     }
 
-    public static void export(String fileName, MetadataSources metadata, ServiceRegistry serviceRegistry)
+    private static void export(String fileName, MetadataSources metadata, ServiceRegistry serviceRegistry)
     {
         SchemaExport schemaExport = new SchemaExport();
         schemaExport.setHaltOnError(false);
         schemaExport.setFormat(true);
         schemaExport.setDelimiter(";");
-        schemaExport.setOutputFile("db-schema.sql");
+        schemaExport.setOutputFile(fileName);
         schemaExport.execute(EnumSet.of(TargetType.SCRIPT), SchemaExport.Action.CREATE, metadata.buildMetadata(), serviceRegistry);
     }
 }
