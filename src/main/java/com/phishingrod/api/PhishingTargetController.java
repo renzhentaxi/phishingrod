2ffd6aa94b0810082ceeac3698d269e3eb8ebb20
renzhentaxibaerde <renzhentaxibaerde@gmail.com>
2018-04-16 23:31:28 -0400

    added api/phishingTarget/get?id={id} for getting a specific target in json. added api/phishingTarget/all for getting all targets

--- src/main/java/com/phishingrod/api/PhishingTargetController.java
+++ src/main/java/com/phishingrod/api/PhishingTargetController.java
@@ -3,7 +3,11 @@ package com.phishingrod.api;
 import com.phishingrod.domain.PhishingTarget;
 import com.phishingrod.services.PhishingTargetService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/phishingTarget")
@@ -18,16 +22,27 @@ public class PhishingTargetController
     }
 
     @PostMapping("add")
-    @ResponseBody
     public boolean add(@RequestParam("emailAddress") String emailAddress)
     {
         return service.add(new PhishingTarget(emailAddress));
     }
 
-    @PostMapping("delete")
-    @ResponseBody
+    @PostMapping("del")
     public boolean delete(@RequestParam("id") long id)
     {
         return service.delete(id);
+    }
+
+    @GetMapping("get")
+    public ResponseEntity<PhishingTarget> get(@RequestParam("id") long id)
+    {
+        Optional<PhishingTarget> target = service.get(id);
+        return target.map(phishingTarget -> new ResponseEntity<>(phishingTarget, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping("all")
+    public Iterable<PhishingTarget> all()
+    {
+        return service.getAll();
     }
 }
