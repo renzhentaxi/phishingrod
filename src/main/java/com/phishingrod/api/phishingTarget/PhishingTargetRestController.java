53fb44d88098c1d3b2b7ea5eec57b658d95dd2e5
renzhentaxibaerde <renzhentaxibaerde@gmail.com>
2018-04-20 23:31:33 -0400

    added a demo of how to use the rest-api to create a table
    also allowed cross origin for now

--- src/main/java/com/phishingrod/api/phishingTarget/PhishingTargetRestController.java
+++ src/main/java/com/phishingrod/api/phishingTarget/PhishingTargetRestController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/phishingTarget")
 public class PhishingTargetRestController
