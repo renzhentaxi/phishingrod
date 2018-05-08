3b97d5ece6024babe50c05a6e024c304469a0bf5
renzhentaxibaerde <renzhentaxibaerde@gmail.com>
2018-05-08 17:12:46 -0400

    fixed a tiny bug that breaks modifying senderService when the restAPI does not provide id, time and other data

--- src/main/java/com/phishingrod/core/service/CrudServices/SenderServerService.java
+++ src/main/java/com/phishingrod/core/service/CrudServices/SenderServerService.java
@@ -2,8 +2,8 @@ package com.phishingrod.core.service.CrudServices;
 
 import com.phishingrod.core.domain.SenderServer;
 import com.phishingrod.core.repository.SenderServerRepository;
-import com.phishingrod.core.service.serviceAddons.StatTrackerAddonProvider;
 import com.phishingrod.core.service.base.NameKeyedEntityService;
+import com.phishingrod.core.service.serviceAddons.StatTrackerAddonProvider;
 import org.springframework.stereotype.Service;
 
 import static com.phishingrod.core.service.base.EntityServiceAddonType.preAdd;
@@ -15,7 +15,16 @@ public class SenderServerService extends NameKeyedEntityService<SenderServer, Se
     public SenderServerService(SenderServerRepository repository)
     {
         super(repository, "Sender Server");
+        registerAddon(preMod, this::merge);
         registerAddon(preAdd, StatTrackerAddonProvider::onCreate);
         registerAddon(preMod, StatTrackerAddonProvider::onUpdate);
+    }
+
+    private SenderServer merge(SenderServer change)
+    {
+        SenderServer original = find(change.getId());
+        if (change.getHost() != null) original.setHost(change.getHost());
+        original.setPort(change.getPort());
+        return original;
     }
 }
